77b1be33acdc8c0246015c4c8ddfac196ed5a4ed
DarthRomolus <rom.mail2005@gmail.com>
2025-11-22 17:45:31 +0200

    project init#4

---
+++ apps/orbital-service/src/satellite/satellite.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { SatelliteController } from './satellite.controller';
+import { SatelliteService } from './satellite.service';
+
+@Module({
+  controllers: [SatelliteController],
+  providers: [SatelliteService]
+})
+export class SatelliteModule {}
