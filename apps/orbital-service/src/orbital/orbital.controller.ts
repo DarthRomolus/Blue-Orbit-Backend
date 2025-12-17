eefca328a79ce6d1fdda2c2c8a7d77fb3bf8fe53
DarthRomolus <rom.mail2005@gmail.com>
2025-12-17 19:58:44 +0200

    Feat(orbital): Create new endpoint for all satellite data and refine processing endpoint

--- apps/orbital-service/src/orbital/orbital.controller.ts
+++ apps/orbital-service/src/orbital/orbital.controller.ts
@@ -1,15 +1,19 @@
 import { Controller } from '@nestjs/common';
 import { OrbitalService } from './orbital.service';
 import { Get } from '@nestjs/common';
-import { Request } from 'express';
 import { SatelliteData } from '@generated/orbital-client';
 
 @Controller('satellites')
 export class OrbitalController {
   constructor(private readonly orbitalService: OrbitalService) {}
 
-  @Get()
+  @Get('process')
   async getTLE(): Promise<SatelliteData[]> {
+    //dev endpoint
     return await this.orbitalService.processTleData();
+  }
+  @Get()
+  async getAllSatellitesData(): Promise<SatelliteData[] | null> {
+    return await this.orbitalService.allSatellitesData();
   }
 }
