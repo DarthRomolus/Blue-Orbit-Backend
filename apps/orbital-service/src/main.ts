facc45fdc9c9558c4f759b551784de10c8146a8d
DarthRomolus <rom.mail2005@gmail.com>
2026-01-20 14:04:25 +0200

    feat(orbital-service): Configure as RabbitMQ microservice

--- apps/orbital-service/src/main.ts
+++ apps/orbital-service/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: true });
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'], // הכתובת של הראביט בדוקר
+      queue: 'orbital_queue', // השם של התור (חשוב!)
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
