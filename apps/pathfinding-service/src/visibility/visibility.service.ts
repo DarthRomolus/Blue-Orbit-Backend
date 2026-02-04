bd587b37f86c8556c5eaadc1ed9332c5170ac6e7
DarthRomolus <rom.mail2005@gmail.com>
2026-02-04 20:53:50 +0200

    feat(visibility): Implement momentary and maximum coverage score calculation

--- apps/pathfinding-service/src/visibility/visibility.service.ts
+++ apps/pathfinding-service/src/visibility/visibility.service.ts
@@ -26,18 +26,122 @@ export class VisibilityService {
       height: satellite.height,
     };
   }
-  // ------------------------------------------------------------------DEV
-  async checkRMQ() {
-    return await this.orbitalClientService.getReducedAllSatelliteInfo();
+  private calculateSatellitePositionBySatrec(
+    satrec: satellite.SatRec,
+    date: Date,
+  ): SatellitePositionGeodetic | undefined {
+    const positionAndVelocity = satellite.propagate(satrec, date);
+    if (
+      !positionAndVelocity?.position ||
+      typeof positionAndVelocity.position !== 'object'
+    ) {
+      return undefined;
+    }
+    const positionEci: satellite.EciVec3<number> = positionAndVelocity.position;
+    const gmst = satellite.gstime(date);
+    const positionGdRadians: SatellitePositionGeodetic =
+      satellite.eciToGeodetic(positionEci, gmst);
+    const positionGdDegrees: SatellitePositionGeodetic = {
+      latitude: satellite.radiansToDegrees(positionGdRadians.latitude),
+      longitude: satellite.radiansToDegrees(positionGdRadians.longitude),
+      height: positionGdRadians.height,
+    };
+    return positionGdDegrees;
   }
+  async momentaryCoverageScore(
+    startDate: Date,
+    endDate: Date,
+    locationCenter: Coordinates,
+    locationRadiusKm: number,
+  ): Promise<Map<number, number>> {
+    const momentaryCoverageScore: Map<number, number> = new Map();
+    let currentTime: Date = new Date(startDate);
+
+    const reducedSatelliteData: ReducedSatelliteData[] =
+      await this.orbitalClientService.getReducedAllSatelliteInfo();
+
+    const satrecs: satellite.SatRec[] = [];
+    for (const data of reducedSatelliteData) {
+      try {
+        const satrec = satellite.twoline2satrec(data.line1, data.line2);
+        satrecs.push(satrec);
+      } catch {}
+    }
+
+    while (currentTime <= endDate) {
+      const timestamp = currentTime.getTime();
+      momentaryCoverageScore.set(timestamp, 0);
+      let timeWindowScore = 0;
+
+      for (const satrec of satrecs) {
+        const positionGd = this.calculateSatellitePositionBySatrec(
+          satrec,
+          currentTime,
+        );
+        if (!positionGd) {
+          continue;
+        }
+        const satelliteCoverageCenter: Coordinates = {
+          latitude: positionGd.latitude,
+          longitude: positionGd.longitude,
+        };
+        const satelliteCoverageRadius = calculateEffectiveRadius(
+          positionGd.height,
+        );
+        const satelliteScore = calculateCoverageScore(
+          locationCenter,
+          locationRadiusKm,
+          satelliteCoverageCenter,
+          satelliteCoverageRadius,
+        );
+
+        timeWindowScore += satelliteScore;
+      }
+      momentaryCoverageScore.set(timestamp, timeWindowScore);
+      currentTime.setMinutes(currentTime.getMinutes() + 2);
+    }
+
+    return momentaryCoverageScore;
+  }
+  //------------------------------------------------dev(make sliding window)---------------
   async calculateMaxCoverageTimeWindow(
     startDate: Date,
     endDate: Date,
-    timeFrameHours: number,
     locationCenter: Coordinates,
     locationRadiusKm: number,
-  ) {
-    const reducedSatelliteInfo: ReducedSatelliteInfo[] =
-      await this.orbitalClientService.getReducedAllSatelliteInfo();
+    timeFrameHours: number,
+  ): Promise<TimeWindowScore> {
+    const timeFrameMinute =
+      timeFrameHours *
+      TIME_DEFUALTS.HOURS_TO_MINUTES *
+      TIME_DEFUALTS.TWO_MINUTES_TIME_FRAMES;
+    let maxCoverageTimeWindow: TimeWindowScore = {
+      startTime: null,
+      coverageScore: 0,
+    };
+    const coverageScoreMap = await this.momentaryCoverageScore(
+      startDate,
+      endDate,
+      locationCenter,
+      locationRadiusKm,
+    );
+    const entries: TimeWindowScore[] = Array.from(
+      coverageScoreMap,
+      ([timestamp, coverageScore]) => {
+        return { startTime: new Date(timestamp), coverageScore };
+      },
+    );
+    for (let i = 0; i < entries.length; i++) {
+      let momentaryCoverageScoreSum = 0;
+      for (let j = i; j < i + timeFrameMinute; j++) {
+        momentaryCoverageScoreSum =
+          momentaryCoverageScoreSum + entries[j].coverageScore;
+      }
+      if (momentaryCoverageScoreSum > maxCoverageTimeWindow.coverageScore) {
+        maxCoverageTimeWindow.coverageScore = momentaryCoverageScoreSum;
+        maxCoverageTimeWindow.startTime = entries[i].startTime;
+      }
+    }
+    return maxCoverageTimeWindow;
   }
 }
