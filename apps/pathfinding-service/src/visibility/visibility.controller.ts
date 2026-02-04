358cc73c1fd3ad9a680e4cfd98a8d4c703a9d801
DarthRomolus <rom.mail2005@gmail.com>
2026-02-04 20:53:51 +0200

    chore(visibility): Remove development test endpoint

--- apps/pathfinding-service/src/visibility/visibility.controller.ts
+++ apps/pathfinding-service/src/visibility/visibility.controller.ts
@@ -4,9 +4,4 @@ import { VisibilityService } from './visibility.service';
 @Controller('visibility')
 export class VisibilityController {
   constructor(private readonly visibilityService: VisibilityService) {}
-
-  @Get('/test') //dev
-  public async test() {
-    return await this.visibilityService.checkRMQ();
-  }
 }
