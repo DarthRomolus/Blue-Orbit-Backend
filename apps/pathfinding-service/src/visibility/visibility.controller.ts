abe9b6d039b62b1dbbb8d3ca8cf20bccda3746fa
DarthRomolus <rom.mail2005@gmail.com>
2026-02-05 19:55:05 +0200

    Feat: Add dev test endpoint for visibility calculation

--- apps/pathfinding-service/src/visibility/visibility.controller.ts
+++ apps/pathfinding-service/src/visibility/visibility.controller.ts
@@ -1,7 +1,41 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { VisibilityService } from './visibility.service';
+import { TimeWindowScore } from 'src/common/types/timeWindowScore';
 
 @Controller('visibility')
 export class VisibilityController {
   constructor(private readonly visibilityService: VisibilityService) {}
+  //---------------------------------------------DEV------------------------------------
+  @Get('test')
+  async runSanityTest() {
+    // 1. הגדרת משתנים (Hardcoded) לבדיקה
+    const startDate = new Date(); // עכשיו
+    const endDate = new Date(startDate.getTime() + 7 * 24 * 60 * 60 * 1000); // עוד 4 שעות
+
+    const telAvivCoords = { latitude: 32.0853, longitude: 34.7818 };
+    const radiusKm = 500; // רדיוס גדול כדי בטוח לתפוס משהו
+    const timeFrameHours = 1; // מחפשים את החלון הכי טוב של שעה אחת
+
+    console.log('--- מפעיל בדיקה ידנית ---');
+    console.log(
+      `בודק בין ${startDate.toISOString()} לבין ${endDate.toISOString()}`,
+    );
+
+    // 2. הפעלת הסרביס
+    const result = await this.visibilityService.calculateMaxCoverageTimeWindow(
+      startDate,
+      endDate,
+      telAvivCoords,
+      radiusKm,
+      timeFrameHours,
+    );
+
+    // 3. החזרת התשובה ישר לדפדפן
+    return {
+      message: 'Test Complete',
+      inputs: { startDate, endDate, radiusKm },
+      result: result,
+    };
+  }
+  //---------------------------------------------DEV------------------------------------
 }
