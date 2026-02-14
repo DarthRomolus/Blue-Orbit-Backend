6720ef2e74deda2842bf719d2099f80c42975578
DarthRomolus <rom.mail2005@gmail.com>
2026-02-14 14:22:33 +0200

    api gateway init restart

---
+++ apps/api-gateway-java/src/main/java/com/blueorbit/apigatewayjava/ApiGatewayJavaApplication.java
@@ -0,0 +1,13 @@
+package com.blueorbit.apigatewayjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayJavaApplication.class, args);
+    }
+
+}
