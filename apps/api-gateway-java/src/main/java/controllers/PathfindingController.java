3a1b269b321e366de6b148f31ad95f65c4c727f0
DarthRomolus <rom.mail2005@gmail.com>
2026-02-14 18:26:39 +0200

    feat: Add /api/pathfinding/visibility endpoint

---
+++ apps/api-gateway-java/src/main/java/controllers/PathfindingController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+
+import dtos.VisibilityRequest;
+import services.PathfindingGatewayService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/pathfinding")
+public class PathfindingController {
+
+    @Autowired
+    private PathfindingGatewayService gatewayService;
+
+    @PostMapping("/visibility")
+    public Object checkVisibility(@RequestBody VisibilityRequest request) {
+        return gatewayService.calculateCoverage(request);
+    }
+}
