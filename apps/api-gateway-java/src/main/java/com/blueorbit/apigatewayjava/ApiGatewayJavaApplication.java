6ffa6e8abf4262d9cf710dcafdc316d5121ed217
DarthRomolus <rom.mail2005@gmail.com>
2026-02-24 18:33:41 +0200

    Chore: Remove trailing whitespace in Java gateway application

--- apps/api-gateway-java/src/main/java/com/blueorbit/apigatewayjava/ApiGatewayJavaApplication.java
+++ apps/api-gateway-java/src/main/java/com/blueorbit/apigatewayjava/ApiGatewayJavaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiGatewayJavaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayJavaApplication.class, args);
     }
