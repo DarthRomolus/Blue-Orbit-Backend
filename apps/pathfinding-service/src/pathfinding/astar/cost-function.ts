96f6e807f493104364c5827e5a08c9c72b53765c
DarthRomolus <rom.mail2005@gmail.com>
2026-02-24 18:33:46 +0200

    Refactor(pathfinding): Introduce A* node score calculation

--- apps/pathfinding-service/src/pathfinding/astar/cost-function.ts
+++ apps/pathfinding-service/src/pathfinding/astar/cost-function.ts
@@ -1,11 +1,27 @@
 import { Coordinates } from 'src/common/types/coordinates';
 import { heuristic } from './heuristic';
 import { State } from '../graph/state';
+import { edgeCostFunction } from './edge-cost';
+import * as satellite from 'satellite.js';
 
-export function costFunction(
-  pathCost: number,
+/**
+calculates f cost of every node
+*/
+export function calculateNodeScores(
+  ChildNodeState: State,
   goal: Coordinates,
-  currentState: State,
+  satellites: satellite.SatRec[],
 ) {
-  const distancePenalty = heuristic(currentState, goal);
+  const hScore = heuristic(ChildNodeState, goal);
+
+  const stepCost = edgeCostFunction(ChildNodeState, satellites);
+
+  const gScore = ChildNodeState.costToPoint + stepCost;
+
+  const fScore = gScore + hScore;
+
+  return {
+    gScore,
+    fScore,
+  };
 }
