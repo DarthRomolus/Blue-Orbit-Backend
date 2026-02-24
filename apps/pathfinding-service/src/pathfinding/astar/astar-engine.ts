81dba90aa226f21fda28d1961b439972af4fbd7d
DarthRomolus <rom.mail2005@gmail.com>
2026-02-24 18:30:17 +0200

    started work on A* engine, added StatesList type

--- apps/pathfinding-service/src/pathfinding/astar/astar-engine.ts
+++ apps/pathfinding-service/src/pathfinding/astar/astar-engine.ts
@@ -1,6 +1,41 @@
-import { State } from '../graph/state';
-
-export function astarEngine(initialState: State) {
-  let closedStates: State[];
-  let openState: State[];
+import { Coordinates } from 'src/common/types/coordinates';
+import type { State, StatesList, ChildrenStates } from '../graph/state';
+import * as satellite from 'satellite.js';
+import { PATHFINDING_DEFAULTS } from 'src/common/constants/pathfinding.constants';
+import { nodesBuilder } from '../graph/nodes-builder';
+import { calculateNodeScores } from './cost-function';
+export function astarEngine(
+  initialState: State,
+  goal: Coordinates,
+  satellites: satellite.SatRec[],
+) {
+  let finalState: State;
+  let closedList: StatesList[] = [];
+  let openList: StatesList[] = [];
+  let currentState: State = {
+    ...initialState,
+  };
+  let iterations = 0;
+  while (
+    currentState.latitude === goal.latitude &&
+    currentState.longitude === goal.longitude &&
+    iterations <= PATHFINDING_DEFAULTS.MAX_ITERATIONS
+  ) {
+    let childrenStates: ChildrenStates = nodesBuilder(currentState);
+    let leftChildCost = calculateNodeScores(
+      childrenStates.left,
+      goal,
+      satellites,
+    );
+    let rightChildCost = calculateNodeScores(
+      childrenStates.right,
+      goal,
+      satellites,
+    );
+    let straightChildCost = calculateNodeScores(
+      childrenStates.straight,
+      goal,
+      satellites,
+    );
+  }
 }
