43da62ba88f92afb58e80118d0b98b7e2631935c
DarthRomolus <rom.mail2005@gmail.com>
2026-03-02 19:34:42 +0200

    feat(position): Remove HTTP GET endpoints from PositionController

--- apps/orbital-service/src/position/position.controller.ts
+++ apps/orbital-service/src/position/position.controller.ts
@@ -6,17 +6,6 @@ import { Payload } from '@nestjs/microservices';
 @Controller('position')
 export class PositionController {
   constructor(private readonly positionService: PositionService) {}
-  @Get(':id')
-  public calculateSatellitePosition(@Param('id') noradId: string) {
-    return this.positionService.calculateSatellitePositionById(
-      noradId,
-      new Date(),
-    );
-  }
-  @Get('path/:id') //dev
-  public async test(@Param('id') noradId: string) {
-    return await this.positionService.calculateSatellitePath(noradId);
-  }
   @MessagePattern({ cmd: 'satellite_position' })
   public calculatePositionForRMQ(@Payload() data: string) {
     const noradID = data;
