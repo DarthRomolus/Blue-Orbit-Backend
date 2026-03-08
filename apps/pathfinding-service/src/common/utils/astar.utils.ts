9f25eb218dee9118cfc2adb0fb75dfb9a8d35532
DarthRomolus <rom.mail2005@gmail.com>
2026-03-08 09:18:30 +0200

    refactor: Format `astar.utils.ts` for readability

--- apps/pathfinding-service/src/common/utils/astar.utils.ts
+++ apps/pathfinding-service/src/common/utils/astar.utils.ts
@@ -33,14 +33,16 @@ export function shouldForceMicroSteps(
 ): boolean {
   if (
     PATHFINDING_DEFAULTS.W_CONN <= 0 ||
-    currentState.signalQuality < PATHFINDING_DEFAULTS.ZOOM_IN_SIGNAL_THRESHOLD ||
+    currentState.signalQuality <
+      PATHFINDING_DEFAULTS.ZOOM_IN_SIGNAL_THRESHOLD ||
     distanceToGoal <= PATHFINDING_DEFAULTS.DYNAMIC_STEP_DISTANCE_THRESHOLD_KM
   ) {
     return false;
   }
 
   const macroDistanceKm =
-    (PATHFINDING_DEFAULTS.DEFAULT_SPEED_KMH / PATHFINDING_DEFAULTS.SECONDS_PER_HOUR) * 
+    (PATHFINDING_DEFAULTS.DEFAULT_SPEED_KMH /
+      PATHFINDING_DEFAULTS.SECONDS_PER_HOUR) *
     PATHFINDING_DEFAULTS.DYNAMIC_STEP_FAST_SECONDS;
 
   const probeCoords = calculateDestination(
@@ -51,7 +53,8 @@ export function shouldForceMicroSteps(
 
   const probeTime = new Date(
     currentState.time.getTime() +
-      PATHFINDING_DEFAULTS.DYNAMIC_STEP_FAST_SECONDS * PATHFINDING_DEFAULTS.MS_PER_SECOND,
+      PATHFINDING_DEFAULTS.DYNAMIC_STEP_FAST_SECONDS *
+        PATHFINDING_DEFAULTS.MS_PER_SECOND,
   );
 
   const probeEcf = getOrComputeEcfPositions(probeTime, ecfCache, satrecs);
@@ -65,7 +68,9 @@ export function shouldForceMicroSteps(
 
   const probeResult = edgeCostFunction(probeState, probeEcf, macroDistanceKm);
 
-  return probeResult.signalQuality < PATHFINDING_DEFAULTS.ZOOM_IN_SIGNAL_THRESHOLD;
+  return (
+    probeResult.signalQuality < PATHFINDING_DEFAULTS.ZOOM_IN_SIGNAL_THRESHOLD
+  );
 }
 
 export function getOrComputeEcfPositions(
@@ -75,11 +80,11 @@ export function getOrComputeEcfPositions(
 ): satellite.EcfVec3<number>[] {
   const timeMs = time.getTime();
   let ecfPositions = ecfCache.get(timeMs);
-  
+
   if (!ecfPositions) {
     ecfPositions = propagateSatellitesToEcf(satrecs, time);
     ecfCache.set(timeMs, ecfPositions);
   }
-  
+
   return ecfPositions;
 }
